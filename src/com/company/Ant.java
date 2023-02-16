c6d8fdd20742f7d839d8a5b5231a96bbf5dac183
Szymon Rogoziński <pokeszymon.pl@gmail.com>
2023-02-16 21:23:21 +0100

    Add seed panel and increase code quality

---
+++ src/com/company/Ant.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Ant {
+    private final int x,y;
+
+    public Ant(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
